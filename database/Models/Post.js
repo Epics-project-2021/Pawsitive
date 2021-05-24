98613f877447e0d0a34acd53493fcae5abc79677
sparsh229 <sparsh.gupta229@gmail.com>
2021-05-25 00:32:56 +0530

    complete content.js and edit.js paths

--- database/Models/Post.js
+++ database/Models/Post.js
@@ -1,47 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+  title: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  author: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  requirement: {
+    type: String,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  postedOn: {
+    type: Date,
+    default: Date.now,
+  },
+  edited: {
+    type: Boolean,
+    default: false,
+  },
+  img: {
+    type: String,
+    default: "/images/1.jpg",
+  },
+  comments: [
+    {
+      id: Number,
+      comment: String,
+      username: String,
     },
-    phone: {
-        type: String,
-    },
-    author: {
-        type: String,
-    },
-    name: {
-        type: String,
-    },
-    area: {
-        type: String,
-        required: true,
-    },
-    requirement: {
-        type: String,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    postedOn: {
-        type: Date,
-        default: Date.now,
-    },
-    edited: {
-        type: Boolean,
-        default: false,
-    },
-    img: {
-        type: String,
-        default: '/images/1.jpg',
-    },
-    comments: [
-        {
-            id: Number,
-            comment: String,
-        },
-    ],
+  ],
 });
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
--- routes/profile.js
+++ routes/profile.js
@@ -1,46 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const User = require("../database/Models/User");
+const Post = require("../database/Models/Post");
 //defining sorting criteria for profile page posts
 function compare(a, b) {
-    if (a.postedOn < b.postedOn) {
-        return 1;
-    }
-    if (a.postedOn > b.postedOn) {
-        return -1;
-    }
-    return 0;
+  if (a.postedOn < b.postedOn) {
+    return 1;
+  }
+  if (a.postedOn > b.postedOn) {
+    return -1;
+  }
+  return 0;
 }
 //@route GET /profile/:id
 //@desc Get profile
 //@access Private
-router.get('/:id', async (req, res) => {
-    if (req.isAuthenticated()) {
-        const userId = req.params.id;
+router.get("/:id", async (req, res) => {
+  if (req.isAuthenticated()) {
+    const userId = req.params.id;
 
-        //get user
-        if (userId === req.user.id) {
-            const user = await User.findById(userId);
-            Promise.all(
-                user.posts.map((postId) => {
-                    Post.findById(postId)
-                        .exec()
-                        .catch((err) => console.error(err));
-                })
-            )
-                .then((posts) => {
-                    res.render('profile', {
-                        posts: posts.sort(compare),
-                        req: req,
-                    });
-                })
-                .catch((err) => console.error(err));
-        } else {
-            res.redirect('/auth/signup');
-        }
+    //get user
+    if (userId === req.user.id) {
+      const user = await User.findById(userId);
+      Promise.all(
+        user.posts.map((postId) =>
+          Post.findOne({ _id: postId })
+            .exec()
+            .catch((err) => console.error)
+        )
+      )
+        .then((posts) => {
+          console.log(posts);
+          res.render("profile", {
+            posts: posts.sort(compare),
+            req: req,
+          });
+        })
+        .catch((err) => console.error(err));
     } else {
-        res.redirect('/auth/login');
+      res.redirect("/auth/signup");
     }
+  } else {
+    res.redirect("/auth/login");
+  }
 });
 
 module.exports = router;
